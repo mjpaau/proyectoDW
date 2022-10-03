6080234daae7b7cc03e2da003f380a791d06817a
mjpaau <mjosepablo@outlook.es>
2022-10-02 20:35:00 -0600

    Ajax basico con metodo post

--- app/core/erp/models.py
+++ app/core/erp/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from datetime import datetime
 
+from django.forms import model_to_dict
+
 from core.erp.choices import gender_choices
 
 class Category(models.Model):
@@ -8,6 +10,10 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+
+    def toJSON(self):
+        item = model_to_dict(self)
+        return item
 
     class Meta:
         verbose_name = 'Categoria'
--- app/core/erp/views/category/views.py
+++ app/core/erp/views/category/views.py
@@ -27,7 +27,11 @@ class CategoryListView(ListView):
         return super().dispatch(request, *args, **kwargs)
 
     def post(self, request, *args, **kwargs):
-        data = {'name': 'William'}
+        data = {}
+        try:
+            data = Category.objects.get(pk=request.POST['id']).toJSON()
+        except Exception as e:
+            data['error'] = str(e)
         return JsonResponse(data)
 
     def get_context_data(self, **kwargs):
