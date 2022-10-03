459f092b595e31fbb0a180ae9dfd5a624b23d303
mjpaau <mjosepablo@outlook.es>
2022-10-02 18:17:22 -0600

    reescritura del metodo post

--- app/core/erp/views/category/views.py
+++ app/core/erp/views/category/views.py
@@ -1,5 +1,7 @@
 from django.contrib.auth.decorators import login_required
+from django.http import JsonResponse
 from django.shortcuts import render, redirect
+from django.views.decorators.csrf import csrf_exempt
 from django.views.generic import ListView
 from django.utils.decorators import method_decorator
 from core.erp.models import Category, Product
@@ -20,12 +22,15 @@ class CategoryListView(ListView):
     # def get_queryset(self):
     #     return Category.objects.filter(name__startswith='L')
 
-    @method_decorator(login_required)
+    @method_decorator(csrf_exempt)
     def dispatch(self, request, *args, **kwargs):
         return super().dispatch(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        data = {'name': 'William'}
+        return JsonResponse(data)
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['title'] = 'Listado de Categorias'
-        # context['object_list'] = Product.objects.all()
         return context
