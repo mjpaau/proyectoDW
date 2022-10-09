8fedc77dba787a5e3f2b9d74383fd34f4f3cd71b
mjpaau <mjosepablo@outlook.es>
2022-10-09 12:30:12 -0600

    Se modificó el diseño del form createview

--- app/core/erp/forms.py
+++ app/core/erp/forms.py
@@ -1,12 +1,31 @@
-from django.forms import ModelForm
+from django.forms import ModelForm, TextInput, Textarea
 
 from core.erp.models import Category
 
 
 class CategoryForm(ModelForm):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # for form in self.visible_fields():
+        #     form.field.widget.attrs['class'] = 'form-control'
+        #     form.field.widget.attrs['autocomplete'] = 'off'
+        self.fields['name'].widget.attrs['autofocus'] = True
+
     class Meta:
         model = Category
         fields = '__all__'
-        labels = {
-            'name': 'Descripción'
+        widgets = {
+            'name': TextInput(
+                attrs={
+                    'placeholder': 'Ingrese un nombre',
+                }
+            ),
+            'desc': Textarea(
+                attrs={
+                    'placeholder': 'Ingrese una descripcion',
+                    'rows': 3,
+                    'cols': 3
+                }
+            )
         }
