60413e750cbbf918eb66412c086e669f37c772d9
mjpaau <mjosepablo@outlook.es>
2022-10-10 17:18:56 -0600

    se agregaron los forms create, update y delete

--- app/core/erp/views/category/views.py
+++ app/core/erp/views/category/views.py
@@ -3,7 +3,7 @@ from django.http import JsonResponse, HttpResponseRedirect
 from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
 from django.views.decorators.csrf import csrf_exempt
-from django.views.generic import ListView, CreateView
+from django.views.generic import ListView, CreateView, UpdateView, DetailView
 from django.utils.decorators import method_decorator
 
 from core.erp.forms import CategoryForm
@@ -17,6 +17,7 @@ def category_list(request):
         'categories': Category.objects.all()
     }
     return render(request,'category/list.html', data)
+
 
 class CategoryListView(ListView):
     model = Category
@@ -50,19 +51,82 @@ class CategoryCreateView(CreateView):
     success_url = reverse_lazy('erp:category_list')
 
     def post(self, request, *args, **kwargs):
-        print(request.POST)
-        form = CategoryForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return HttpResponseRedirect(self.success_url)
-        self.object = None
-        context = self.get_context_data(**kwargs)
-        context['form'] = form
-        return render(request, self.template_name, context)
+        data = {}
+        try:
+            action = request.POST['action']
+            if action == 'add':
+                form = self.get_form()
+                data = form.save()
+            else:
+                data['error'] = 'No ha ingresado ninguna opción'
+            # data = Category.objects.get(pk=request.POST['id']).toJSON()
+        except Exception as e:
+            data['error'] = str(e)
+        return JsonResponse(data)
+
+    # print(request.POST)
+    # form = CategoryForm(request.POST)
+    # if form.is_valid():
+    #     form.save()
+    #     return HttpResponseRedirect(self.success_url)
+    # self.object = None
+    # context = self.get_context_data(**kwargs)
+    # context['form'] = form
+    # return render(request, self.template_name, context)
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['title'] = 'Creacion de Categoría'
+        context['entity'] = 'Categorias'
+        context['list_url'] = reverse_lazy('erp:category_list')
+        context['action'] = 'add'
+        return context
+
+
+class CategoryUpdateView(UpdateView):
+    model = Category
+    form_class = CategoryForm
+    template_name = 'category/create.html'
+    success_url = reverse_lazy('erp:category_list')
+
+    def dispatch(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        return super().dispatch(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        data = {}
+        try:
+            action = request.POST['action']
+            if action == 'edit':
+                form = self.get_form()
+                data = form.save()
+            else:
+                data['error'] = 'No ha ingresado ninguna opción'
+        except Exception as e:
+            data['error'] = str(e)
+        return JsonResponse(data)
+
+    def get_context_data(self, **kwargs):
+        # print(self.object)
+        # print(self.get_object())
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Edicion de Categoría'
+        context['entity'] = 'Categorias'
+        context['list_url'] = reverse_lazy('erp:category_list')
+        context['action'] = 'edit'
+        return context
+
+
+class CategoryDeleteView(DetailView):
+    model = Category
+    template_name = 'category/delete.html'
+    success_url = reverse_lazy('erp_category_list')
+
+    def get_context_data(self, **kwargs):
+        # print(self.object)
+        # print(self.get_object())
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Eliminacion de Categoría'
         context['entity'] = 'Categorias'
         context['list_url'] = reverse_lazy('erp:category_list')
         return context
