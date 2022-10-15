015d81d46d5c90361bee2d33aaad62897f0ae2cc
mjpaau <mjosepablo@outlook.es>
2022-10-15 16:54:27 -0600

    Se agragó un nuevo estilo de vista

--- app/core/erp/views/category/views.py
+++ app/core/erp/views/category/views.py
@@ -1,22 +1,11 @@
-from django.contrib.auth.decorators import login_required
-from django.http import JsonResponse, HttpResponseRedirect
-from django.shortcuts import render, redirect
+from django.http import JsonResponse
 from django.urls import reverse_lazy
 from django.views.decorators.csrf import csrf_exempt
 from django.views.generic import ListView, CreateView, UpdateView, DetailView
 from django.utils.decorators import method_decorator
 
 from core.erp.forms import CategoryForm
-from core.erp.models import Category, Product
-
-
-def category_list(request):
-
-    data = {
-        'title': 'Listado de Categorías',
-        'categories': Category.objects.all()
-    }
-    return render(request,'category/list.html', data)
+from core.erp.models import Category
 
 
 class CategoryListView(ListView):
