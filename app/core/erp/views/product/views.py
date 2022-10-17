19fb892ae9d7dff9657ff8e68b8315638efca98e
mjpaau <mjosepablo@outlook.es>
2022-10-17 16:50:53 -0600

    Se trabajó con media files y ajax, y pequeñas modificacines con respecto a los campos de la tb product

--- app/core/erp/views/product/views.py
+++ app/core/erp/views/product/views.py
@@ -37,18 +37,18 @@ class ProductCreateView(CreateView):
     def dispatch(self, request, *args, **kwargs):
         return super().dispatch(request, *args, **kwargs)
 
-    # def post(self, request, *args, **kwargs):
-    #     data = {}
-    #     try:
-    #         action = request.POST['action']
-    #         if action == 'add':
-    #             form = self.get_form()
-    #             data = form.save()
-    #         else:
-    #             data['error'] = 'No ha ingresado a ninguna opción'
-    #     except Exception as e:
-    #         data['error'] = str(e)
-    #     return JsonResponse(data)
+    def post(self, request, *args, **kwargs):
+        data = {}
+        try:
+            action = request.POST['action']
+            if action == 'add':
+                form = self.get_form()
+                data = form.save()
+            else:
+                data['error'] = 'No ha ingresado a ninguna opción'
+        except Exception as e:
+            data['error'] = str(e)
+        return JsonResponse(data)
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
