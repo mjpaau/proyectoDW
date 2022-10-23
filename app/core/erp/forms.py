d2d11c039196cdc24cb2943ddacd02d29b0413b5
mjpaau <mjosepablo@outlook.es>
2022-10-22 22:41:53 -0600

    Maquetación de factura

--- app/core/erp/forms.py
+++ app/core/erp/forms.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 
 from django.forms import *
-from core.erp.models import Category, Product, Client
+from core.erp.models import Category, Product, Client, Sale
 
 
 class CategoryForm(ModelForm):
@@ -100,6 +100,7 @@ class TestForm(Form):
         'style': 'width: 100%'
     }))
 
+
 class ClientForm(ModelForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -155,3 +156,30 @@ class ClientForm(ModelForm):
     #         raise forms.ValidationError('Validacion xxx')
     #         # self.add_error('name', 'Le faltan caracteres')
     #     return cleaned
+
+
+class SaleForm(ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for form in self.visible_fields():
+            form.field.widget.attrs['class'] = 'form-control'
+            form.field.widget.attrs['autocomplete'] = 'off'
+        self.fields['cli'].widget.attrs['autofocus'] = True
+        self.fields['cli'].widget.attrs['class'] = 'form-control select2'
+        self.fields['cli'].widget.attrs['style'] = 'width: 100%'
+
+    class Meta:
+        model = Sale
+        fields = '__all__'
+        widgets = {
+            'cli': Select(attrs={
+                'class': 'form-control select2',
+                'style': 'width: 100%'
+            }),
+            'date_joined': DateInput(format='%Y-%m-%d',
+               attrs={
+                   'class': 'form-control',
+                   'value': datetime.now().strftime('%Y-%m-%d'),
+               }
+            ),
+        }
