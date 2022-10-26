dddc3d3f461708762bde305d27e934396ebab840
mjpaau <mjosepablo@outlook.es>
2022-10-25 20:58:47 -0600

    Creacion de pdf para factura

--- app/core/erp/views/sale/views.py
+++ app/core/erp/views/sale/views.py
@@ -211,17 +211,51 @@ class SaleDeleteView(LoginRequiredMixin, ValidatePermissionRequiredMixin, Delete
 
 
 class SaleInvoicePdfView(View):
+
+    def link_callback(self, uri, rel):
+        """
+        Convert HTML URIs to absolute system paths so xhtml2pdf can access those
+        resources
+        """
+        # use short variable names
+        sUrl = settings.STATIC_URL  # Typically /static/
+        sRoot = settings.STATIC_ROOT  # Typically /home/userX/project_static/
+        mUrl = settings.MEDIA_URL  # Typically /static/media/
+        mRoot = settings.MEDIA_ROOT  # Typically /home/userX/project_static/media/
+
+        # convert URIs to absolute system paths
+        if uri.startswith(mUrl):
+            path = os.path.join(mRoot, uri.replace(mUrl, ""))
+        elif uri.startswith(sUrl):
+            path = os.path.join(sRoot, uri.replace(sUrl, ""))
+        else:
+            return uri  # handle absolute uri (ie: http://some.tld/foo.png)
+
+        # make sure that file exists
+        if not os.path.isfile(path):
+            raise Exception(
+                'media URI must start with %s or %s' % (sUrl, mUrl)
+            )
+        return path
+
     def get(self, request, *args, **kwargs):
         try:
-            context = {'title': 'Mi primer pdf'}
+            # data = Sale.objects.get(pk=self.kwargs['pk'])
+            # print(data)
+            context = {
+                'sale': Sale.objects.get(pk=self.kwargs['pk']),
+                'comp': {'name': 'UMG SISTEMAS', 'ruc': '9999999999999', 'address': 'Cobán A.V.'},
+                'icon': '{}{}'.format(settings.MEDIA_URL, 'logo.png')
+            }
             template = get_template('sale/invoice.html')
             html = template.render(context)
             response = HttpResponse(content_type='application/pdf')
             response['Content-Disposition'] = 'attachment; filename="report.pdf"'
             pisaStatus = pisa.CreatePDF(
-                html, dest=response
+                html, dest=response,
+                link_callback=self.link_callback
             )
             return response
-        except:
-            pass
+        except Exception as e:
+            print(e)
         return HttpResponseRedirect(reverse_lazy('erp:sale_list'))
